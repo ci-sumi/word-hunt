efd4ac2f9c949c6eb0c193bcf2147c75feb5b5d3
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-07-01 16:08:39 +0100

    Implement function to read words and hints from a text file.

--- run.py
+++ run.py
@@ -31,12 +31,30 @@ def display_instructions():#function for displaying instructionns
     print("You have 6 attempts to guess the correct word")
     print("Enter a single letter each time and press Enter")
     input("Press Enter to return the main menu\n")
+#Function to read fruits and hints from the text file
+def read_file_from_textfile(filepath):
+    word_hint={}
+    with open(filepath,'r')as file:
+        for line in file:
+            line=line.strip()
+            parts=line.split(":")
+            if len(parts)==2:
+                word=parts[0]
+                hint=parts[1]
+                word_hint[word]=hint
+    return word_hint
+
+
+
 
 def main():
     welcome_message()
+    fruit=read_file_from_textfile('fruits.txt')
+    print(fruit)
+    
     while True:
         try:
-            word_letter = input("Please do enter a letter:")
+            word_letter = input("Please do enter a four letter fruit name:")
             if word_letter=="I".lower():
                 display_instructions()
                 clear_screen()
@@ -44,11 +62,11 @@ def main():
             if word_letter=="hint".lower():
                 print("Hint will be provided")
                 continue
-            if not word_letter.isalpha() or len(word_letter)!=1:
+            if not word_letter.isalpha() or len(word_letter)<=4:
                 raise ValueError
                 
         except ValueError as v:
-            print("Enter a valid letter")
+            print("Enter a valid letters")
         
                 
     
