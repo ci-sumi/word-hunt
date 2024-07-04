579e37c8a9c9de92b63fa71012d8423b45e7c237
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-07-04 12:11:10 +0100

    Refactor custom ASCII art with pyfiglet for title generation

--- run.py
+++ run.py
@@ -1,25 +1,25 @@
 import random
-from colorama import Fore,Style,init #module use to print the colored text in the terminal
+from colorama import Fore,Style,init 
 import os
+import pyfiglet
+from pyfiglet import Figlet
 
 init() # initialize colorama
 
-def print_text(text,color,style=""):#print_text function for centered text with color and style support
+def print_text(text,color,style=""): 
+    """
+    print centered text with color and style support
+    """ 
     print(color+style+text.center(80)+Style.RESET_ALL)
 
 def clear_screen():#function to clear the screen
     os.system('cls' if os.name=='nt' else "clear")  
     
     
-def welcome_message():#welcome function
+def welcome_message():# welcome function
     clear_screen()
-    gamename="wordhunt"
-    print_text(" W   W  OOO  RRRR  DDDD    H   H  U   U  N   N  TTTTT  EEEEE  RRRR   ", Fore.GREEN + Style.BRIGHT)
-    print_text(" W   W O   O R   R D   D   H   H  U   U  NN  N    T    E      R   R  ", Fore.CYAN + Style.BRIGHT)
-    print_text(" W W W O   O RRRR  D   D   HHHHH  U   U  N N N    T    EEEE   RRRR   ", Fore.YELLOW + Style.BRIGHT)
-    print_text(" W W W O   O R  R  D   D   H   H  U   U  N  NN    T    E      R  R   ", Fore.MAGENTA + Style.BRIGHT)
-    print_text("  W W   OOO  R   R DDDD    H   H   UUU   N   N    T    EEEEE  R   R  ", Fore.BLUE + Style.BRIGHT)
-    print("\n")
+    f=Figlet(font='letters')
+    print(f.renderText("word Hunt"))
     name=input("Please do enter your name:\n")
     print("Press 'I' for the instruction")
     print("Try to guess the word,one letter at a time")
@@ -32,11 +32,14 @@ def display_instructions():#function for displaying instructionns
     print("You have 6 attempts to guess the correct word")
     print("Enter a single letter each time and press Enter")
     input("Press Enter to return the main menu\n")
+    
+    
 #Function to read fruits and hints from the text file
 def read_file_from_textfile(filepath):
     word_hint={}
     with open(filepath,'r')as file:
         for line in file:
+            print(line)
             line=line.strip().strip('"').strip(',')
             parts=line.split(":")
             if len(parts)==2:
